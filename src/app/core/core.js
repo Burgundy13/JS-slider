ce400ae5d082032d51cf1a826b9da8136c200fd6
Vlado Ivkovic <ivkovicv90@gmail.com>
2024-01-29 23:24:24 +0100

    Arrows enabled

--- src/app/core/core.js
+++ src/app/core/core.js
@@ -5,4 +5,6 @@ export default function initialize() {
 	this.renderSlides = renderSlides;
 	this.render();
 	this.renderSlides(this.data);
+	this.rightArrow.addEventListener('click', this.moveRight.bind(this));
+	this.leftArrow.addEventListener('click', this.moveLeft.bind(this));
 }
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import './scss/style.scss';
 import core from './app/core/core';
+import { moveLeft, moveRight } from '../src/app/events/events';
 
 class Slider {
 	constructor(el, data, options) {
@@ -7,6 +8,9 @@ class Slider {
 		this.data = data;
 		this.initialize = core;
 		this.previewType = 6;
+		this.countInfinity = 0;
+		this.moveLeft = moveLeft;
+		this.moveRight = moveRight;
 	}
 }
 
